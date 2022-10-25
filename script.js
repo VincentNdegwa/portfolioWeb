106bd9d19e96695740208b24d9d9980842f22fa3
VincentNdegwa <116282941+VincentNdegwa@users.noreply.github.com>
2022-10-25 18:18:26 +0300

    This is my portfolio website.
    This website defines all my skills that i have .

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let menu = document.querySelector('#icon-menu');
+let navbar = document.querySelector('.navbar');
+// let sunMode = document.querySelector('#sun-icon')
+let Mode  = document.getElementById("darkmode")
+
+menu.onclick = () => {
+	navbar.classList.toggle('navbar2');
+};
+
+
+
+
+// Mode.onclick= () =>{
+// 	// Mode.classList.toggle("active1")
+// 	document.body.classList.toggle('active1')
+// }
+// 	if(Mode.classList.contains("bx bx-moon")){
+// 		Mode.classList.replace("bx bx-moon" , "bx bx-sun")
+// 		document.body.classList.toggle("active1")
+// 	}
+// 	else{
+// 		Mode.classList.replace("bx bx-sun","bx bx-moon")
+// 		document.body.classList.toggle("active1")
+// 	}
+// }
